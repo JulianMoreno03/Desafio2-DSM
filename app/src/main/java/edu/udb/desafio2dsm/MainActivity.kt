25c32eed767149210c7bb7d5b802e716350367be
Julian Moreno <juliancastillo204@gmail.com>
2024-09-11 12:27:32 -0600

    Haciendo layout register

--- app/src/main/java/edu/udb/desafio2dsm/MainActivity.kt
+++ app/src/main/java/edu/udb/desafio2dsm/MainActivity.kt
@@ -1,6 +1,8 @@
 package edu.udb.desafio2dsm
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+
+        val btnCrearCuenta = findViewById<Button>(R.id.btnCrearCuenta);
+        // Configura el click listener para navegar a otra actividad
+        btnCrearCuenta.setOnClickListener {
+            goToRegisterActivity()
         }
+    }
+
+
+    // Función para ir a la SecondActivity
+    private fun goToRegisterActivity() {
+        val intent = Intent(this, RegisterActivity::class.java)
+        startActivity(intent)
     }
 }
