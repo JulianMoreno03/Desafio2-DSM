61e2e9d9deff74ee641e1bd8c254f99bf51960b2
Julian Moreno <juliancastillo204@gmail.com>
2024-09-11 15:22:53 -0600

    Funcionalidad registro de usuarios con correo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,6 +34,9 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures{
+        viewBinding= true
+    }
 }
 
 dependencies {
@@ -44,6 +47,7 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.firebase.auth)
+    implementation(libs.google.firebase.auth)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/edu/udb/desafio2dsm/RegisterActivity.kt
+++ app/src/main/java/edu/udb/desafio2dsm/RegisterActivity.kt
@@ -1,20 +1,56 @@
 package edu.udb.desafio2dsm
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
 
 class RegisterActivity : AppCompatActivity() {
+
+    // Referencia a FirebaseAuth
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_register)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        auth = FirebaseAuth.getInstance()
+
+        //obtenemos inputs
+        val emailField = findViewById<EditText>(R.id.email)
+        val passwordField = findViewById<EditText>(R.id.password)
+        //obtenemos btn
+        val registerButton = findViewById<Button>(R.id.btnCrearCuenta)
+
+
+        // Acción del botón de registro
+        registerButton.setOnClickListener {
+            val email = emailField.text.toString().trim()
+            val password = passwordField.text.toString().trim()
+
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+                // Registro de usuario en Firebase gracias al la funcion de firebase
+                auth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            // El registro fue exitoso
+                            Toast.makeText(this, "Usuario registrado con éxito", Toast.LENGTH_SHORT).show()
+                            //PENDIENTE
+                            // startActivity(Intent(this, AnotherActivity::class.java))
+                        } else {
+                            // El registro falló
+                            Toast.makeText(this, "Registro fallido: ${task.exception?.message}", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            } else {
+                Toast.makeText(this, "Por favor, complete todos los campos", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
